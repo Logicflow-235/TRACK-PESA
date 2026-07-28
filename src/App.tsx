b0f24e589b7a0fbefd9591c7db3814151534ab9d
logicflow-235 <theurilinet401@gmail.com>
2026-07-28 07:46:42 +0300

    added logout option

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import TransactionList from "./components/TransactionList"
 import Summary from "./components/Summary"
 import Register from './components/Register'
 import Login from "./components/login"
-function App() {
+import { logout } from "./features/auth/authSlice"
+import { useAppSelector, useAppDispatch } from "./app/hooks"
+export default function App() {
+  const  dispatch =useAppDispatch();
+  const token= useAppSelector((state)=>state.auth.token);
+  const handleLogout =()=>{
+    dispatch(logout())
+  }
   return (
     <div className="min-h-screen bg-gray-950 text-white">
       <div className="max-w-2xl mx-auto px-4 py-8">
@@ -17,9 +24,11 @@ function App() {
         <Summary />
         <AddTransaction />
         <TransactionList />
+        {token &&(
+          <button onClick={handleLogout}className="text-red-400 text-sm">Logout</button>
+        )}
       </div>
     </div>
   )
 }
 
-export default App
