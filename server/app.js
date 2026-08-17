d827bf6039ad2ab804bf06a84073a98503e2733c
logicflow-235 <theurilinet401@gmail.com>
2026-08-17 09:41:50 +0300

    fixes

--- server/app.js
+++ server/app.js
@@ -171,7 +171,7 @@ app.post('/login',  async (req,res)=>{
         const token = jwt.sign(
             {id:user._id, username:user.username },
             process.env.JWT_SECRET,
-            {expiresIn:'1h'}
+            {expiresIn:'24h'}
         );
         res.json ({message:'Login sucessful', token});
     }
